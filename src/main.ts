ce9b08f4b93ac53d8e0f70019d31b878e935a0cd
Georgi Zarkya <zmeyfon@list.ru>
2025-02-24 22:03:48 +0300

    feat: Added ENV files with general settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import appConfig from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(appConfig.port, () => console.log(`Server start on port ${appConfig.port}`));
 }
+
 bootstrap();
